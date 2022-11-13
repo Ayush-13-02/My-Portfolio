cfc5ce591a51a21d353c785777430fe423bb58e4
Ayush-13-02 <ayushsharma13jun@gmail.com>
2022-11-13 13:49:51 +0530

    Portfolio

--- components/Header.js
+++ components/Header.js
@@ -7,9 +7,9 @@ export default function Header({ scrollToAbout, scrollToSkill, scrollToProject, 
     <header className="fixed top-0 w-full clearNav z-50">
       <div className="max-w-5xl mx-auto flex flex-wrap p-5 flex-col md:flex-row">
         <div className="flex flex-row items-center justify-between p-3 md:p-1">
-          <span className="flex text-3xl text-white font-medium mb-4 md:mb-0 uppercase animate-bounce duration-500">
-            <Link href="/">Ayush Sharma</Link>
-          </span>
+        <Link href="/"><span className="flex text-3xl text-white font-medium mb-4 md:mb-0 uppercase animate-bounce duration-500">
+            Ayush Sharma
+          </span></Link>
           <button
             className="text-white pb-4 cursor-pointer leading-none px-3 py-1 md:hidden outline-none focus:outline-none content-end ml-auto"
             type="button"
